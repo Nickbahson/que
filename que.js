d1d7cdc89908c12e1f891edf1373b08bdda2e890
nickbahson <nijoba33@gmail.com>
2022-11-10 17:58:56 +0300

    inital

---
+++ que.js
@@ -0,0 +1,12 @@
+const pathName = window.location.pathname
+console.log("++++++++++++++++ JS ++++++++++++++++++++")
+console.log("++++++++++++++++ JS ++++++++++++++++++++")
+console.log("++++++++++++++++ JS ++++++++++++++++++++")
+
+
+alert('HERE!JS')
+
+console.log(pathName)
+console.log("++++++++++++++++ JS ++++++++++++++++++++")
+console.log("++++++++++++++++ JS ++++++++++++++++++++")
+console.log("++++++++++++++++ JS ++++++++++++++++++++")
