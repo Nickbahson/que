e67c752b360cde6326ba5fe75c383876081fcd86
nickbahson <nijoba33@gmail.com>
2023-01-09 12:18:34 +0300

    added debug code

--- src/causiq.js
+++ src/causiq.js
@@ -1,12 +1,22 @@
-import { Causiq } from '@causiq/sdk';
-
-import "./scss/main.scss"
+import { BatchedTransport, Causiq, sendWithFetch  } from '@causiq/sdk';
 
 (function (  ) {
     'use strict'
-    const c = new Causiq();
-    c.init('CQ-H1A23LDP9ABC', (config, { sendWithConsole }) => ({
-        send: typeof window === 'undefined' ? config.send : sendWithConsole
-    }));
+
+    function uniqueId() {
+        return document.cookie.split(';')
+            .filter((item) => item.trim().startsWith('cart='))[0]?.replace('cart=', '')
+    }
+
+    console.log("++++++++++++++++++ document.currentScript.src +++++++++++++++")
+    console.log("++++++++++++++++++ document.currentScript.src +++++++++++++++")
+    console.log(document.currentScript.src)
+    console.log("++++++++++++++++++ document.currentScript.src +++++++++++++++")
+    console.log("++++++++++++++++++ document.currentScript.src +++++++++++++++")
+
+    const moniker = 'test'
+    const transport = new BatchedTransport(1000, sendWithFetch("http://localhost:3001/v1/track"))
+    const c = new Causiq(transport);
+    client.init(moniker, null, uniqueId)
 
 })()
