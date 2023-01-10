0a59207ecbe0721fdd962e64d9957ad2a2891c69
nickbahson <nijoba33@gmail.com>
2023-01-10 10:44:01 +0300

    updated unique user id mutation

--- src/causiq.js
+++ src/causiq.js
@@ -3,6 +3,9 @@ import { BatchedTransport, Causiq, sendWithFetch  } from '@causiq/sdk';
 (function (  ) {
     'use strict'
 
+    const cqid =
+        document.cookie.split(';').filter((item) => item.trim().startsWith('cqid='))[0]?.replace('cqid=', '')
+
     const uniqueId =
         document.cookie.split(';').filter((item) => item.trim().startsWith('cart='))[0]?.replace('cart=', '')
 
@@ -15,5 +18,9 @@ import { BatchedTransport, Causiq, sendWithFetch  } from '@causiq/sdk';
     const transport = new BatchedTransport(1000, sendWithFetch(trackUrl))
     const client = new Causiq(transport);
     client.init(moniker, null, uniqueId)
+    if ( uniqueId ) {
+        // When uniqueId is available, update the default id to that.
+        client.identify(uniqueId, cqid)
+    }
 
 })()
