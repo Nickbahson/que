60c0af273546accf42372e7756ee231139486d78
nickbahson <nijoba33@gmail.com>
2023-02-08 09:29:13 +0300

    updated identify

--- src/causiq.js
+++ src/causiq.js
@@ -3,29 +3,31 @@ import { BatchedTransport, Causiq, sendWithFetch } from '@causiq/sdk';
 (function (  ) {
     'use strict'
 
-    const uniqueId =
-        document.cookie.split(';').filter((item) => item.trim().startsWith('cart='))[0]?.replace('cart=', '')?.trim()
+    try {
+        const uniqueId =
+            document.cookie.split(';').filter((item) => item.trim().startsWith('cart='))[0]?.replace('cart=', '')?.trim()
 
-    const srcUri = document.currentScript.src
-    const url = new URL(srcUri);
-    const searchParams = new URLSearchParams(url.search);
-    const moniker = searchParams.get('moniker')
-    const trackUrl = searchParams.get('trackUrl')
+        const srcUri = document.currentScript.src
+        const url = new URL(srcUri);
+        const searchParams = new URLSearchParams(url.search);
+        const moniker = searchParams.get('moniker')
+        const trackUrl = searchParams.get('trackUrl')
 
-    const transport = new BatchedTransport(1000, sendWithFetch(trackUrl))
-    const client = new Causiq(transport);
-    client.init(
-        moniker,
-        { shouldCaptureText: function shouldCapture(el) {
-            if ( uniqueId && client.getIdentity() !== uniqueId ) {
-                client.identify(uniqueId)
-            }
-            return true
-            }
-        },
-        uniqueId
-    )
-
-
-
+        const transport = new BatchedTransport(1000, sendWithFetch(trackUrl))
+        const client = new Causiq(transport);
+        client.init(moniker)
+        if ( uniqueId ) client.identify(uniqueId)
+    } catch (e) {
+        console.warn(e)
+    }
+    // client.init(
+    //     moniker,
+    //     { shouldCaptureText: function shouldCapture(el) {
+    //             if ( uniqueId && client.getIdentity() !== uniqueId ) {
+    //                 client.identify(uniqueId)
+    //             }
+    //             return true
+    //         }
+    //     },uniqueId
+    // )
 })()
