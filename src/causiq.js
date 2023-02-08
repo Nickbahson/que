f0aff7352d2617fd6f3455895aa53aa8c19d22ee
nickbahson <nijoba33@gmail.com>
2023-02-08 09:53:19 +0300

    fire event when all page resouces are loaded

--- src/causiq.js
+++ src/causiq.js
@@ -3,31 +3,25 @@ import { BatchedTransport, Causiq, sendWithFetch } from '@causiq/sdk';
 (function (  ) {
     'use strict'
 
-    try {
-        const uniqueId =
-            document.cookie.split(';').filter((item) => item.trim().startsWith('cart='))[0]?.replace('cart=', '')?.trim()
+    window.onload = (event) => {
+        try {
+            const uniqueId =
+                document.cookie.split(';').filter((item) => item.trim().startsWith('cart='))[0]?.replace('cart=', '')?.trim()
 
-        const srcUri = document.currentScript.src
-        const url = new URL(srcUri);
-        const searchParams = new URLSearchParams(url.search);
-        const moniker = searchParams.get('moniker')
-        const trackUrl = searchParams.get('trackUrl')
+            const srcUri = document.currentScript.src
+            const url = new URL(srcUri);
+            const searchParams = new URLSearchParams(url.search);
+            const moniker = searchParams.get('moniker')
+            const trackUrl = searchParams.get('trackUrl')
 
-        const transport = new BatchedTransport(1000, sendWithFetch(trackUrl))
-        const client = new Causiq(transport);
-        client.init(moniker)
-        if ( uniqueId ) client.identify(uniqueId)
-    } catch (e) {
-        console.warn(e)
-    }
-    // client.init(
-    //     moniker,
-    //     { shouldCaptureText: function shouldCapture(el) {
-    //             if ( uniqueId && client.getIdentity() !== uniqueId ) {
-    //                 client.identify(uniqueId)
-    //             }
-    //             return true
-    //         }
-    //     },uniqueId
-    // )
+            const transport = new BatchedTransport(1000, sendWithFetch(trackUrl))
+            const client = new Causiq(transport);
+            client.init(moniker)
+            if ( uniqueId ) client.identify(uniqueId)
+        } catch (e) {
+
+        }
+    };
+
+
 })()
