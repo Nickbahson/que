246ca5b5e2b5dfdadb24c8eaf7524989f1b3911f
nickbahson <nijoba33@gmail.com>
2023-02-14 18:58:50 +0300

    config to not use cookie

--- src/causiq.js
+++ src/causiq.js
@@ -1,27 +1,24 @@
+'use strict'
+
 import { BatchedTransport, Causiq, sendWithFetch } from '@causiq/sdk';
+const causiqInit = (event) => {
+    try {
+        const uniqueId =
+            document.cookie.split(';').filter((item) => item.trim().startsWith('cart='))[0]?.replace('cart=', '')?.trim()
 
-(function (  ) {
-    'use strict'
+        const srcUri = document.currentScript.src
+        const url = new URL(srcUri);
+        const searchParams = new URLSearchParams(url.search);
+        const moniker = searchParams.get('moniker')
+        const trackUrl = searchParams.get('trackUrl')
 
-    const causiqInit = (event) => {
-        try {
-            const uniqueId =
-                document.cookie.split(';').filter((item) => item.trim().startsWith('cart='))[0]?.replace('cart=', '')?.trim()
+        const transport = new BatchedTransport(1000, sendWithFetch(trackUrl))
+        const client = new Causiq(transport);
+        client.init(moniker, {cookieless: true})
+        if ( uniqueId ) client.identify(uniqueId)
+    } catch (e) {
 
-            const srcUri = document.currentScript.src
-            const url = new URL(srcUri);
-            const searchParams = new URLSearchParams(url.search);
-            const moniker = searchParams.get('moniker')
-            const trackUrl = searchParams.get('trackUrl')
+    }
+};
 
-            const transport = new BatchedTransport(1000, sendWithFetch(trackUrl))
-            const client = new Causiq(transport);
-            client.init(moniker)
-            if ( uniqueId ) client.identify(uniqueId)
-        } catch (e) {
-
-        }
-    };
-
-    window.addEventListener('load', causiqInit)
-})()
+window.addEventListener('load', causiqInit)
